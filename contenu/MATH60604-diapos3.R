24313e35a68714d1be56cfe0f84e74853f4bfbed
Léo Belzile <belzilel@gmail.com>
2024-09-15 18:02:38 -0400

    Modifier code diapos 3

--- contenu/MATH60604-diapos3.R
+++ contenu/MATH60604-diapos3.R
@@ -12,9 +12,9 @@ theme_set(theme_classic())
 
 # Données de l'exemple 1
 data(LC19_S1, package = "hecedsm") # charger les données
-?LC19_S1 # description de la base de donnée (menu d'aide)
+?hecedsm::LC19_S1 # description de la base de donnée (menu d'aide)
 str(LC19_S1) # résumé sommaire
-summary(LC19_S1) # satistiques descriptives
+summary(LC19_S1) # statistiques descriptives
 with(LC19_S1, cor(familiarity, prodeval)) # corrélation linéaire
 length(unique(LC19_S1$prodeval)) # nombre de valeurs uniques
 
@@ -26,7 +26,7 @@ ggplot(data = LC19_S1,
 
 # Données de l'exemple 2
 data("BSJ92", package = "hecedsm")
-?BSJ92
+?hecedsm::BSJ92
 # pré-test vs post-test 1
 ggplot(data = BSJ92,
        mapping = aes(x = pretest1,
@@ -44,7 +44,7 @@ summary(BSJ92) # Statistiques descriptives
 
 # Données de l'exemple 3
 data(college, package = "hecmodstat")
-?college
+?hecmodstat::college
 # Différences de salaire notables, mais pas nécessairement à cause du sexe
 t.test(salaire ~ sexe, data = college)
 # Statistiques descriptives
@@ -82,7 +82,7 @@ with(college, cor(annees, service))
 
 # Données 4
 data(MV23_S1, package = "hecedsm")
-?MV23_S1
+?hecedsm::MV23_S1
 str(MV23_S1)
 
 summary(MV23_S1)
@@ -131,7 +131,7 @@ lm(posttest1 ~ group, data = BSJ92, contrasts = list(group = "contr.sum"))
 # Enlever l'ordonnée à l'origine (trois moyennes de groupe)
 lm(posttest1 ~ -1 + group, data = BSJ92)
 # Les erreurs-type des moyennes sont égales dans le cas d'échantillons balancés
-# balance = même nombre d'observations par groupe
+# échantillon balancé = même nombre d'observations par groupe expérimental
 
 # Analyse de covariance modèle linéaire pour post en fonction de pré
 lm(posttest1 ~ group +  pretest1,
@@ -142,10 +142,44 @@ linmod <- lm(
   data = BSJ92 |> 
     dplyr::mutate( # centrer le pré-test
       pretest1 = pretest1 - mean(pretest1)))
+# Seul le coefficient de l'ordonnée à l'origine change
 coef(linmod) 
 # Coefficients de la moyenne
 ## (Intercept)    pretest1   groupDRTA     groupTA 
 ##       6.188       0.693       3.627       2.036
+summary(linmod)
+
 
 
 ## Estimation
+
+# En utilisant la formule des moindres carrés
+# Définir la variable réponse et la matrice du modèle
+y <- BSJ92$posttest1
+X <- model.matrix(linmod)
+mco <- function(X, y){ as.numeric(solve(t(X) %*% X) %*% t(X) %*% y)}
+# Coefficients (MCO) de la moyenne
+betas <- as.numeric(coef(linmod))
+# En pratique, il est préférable d'utiliser une décomposition en valeurs
+# singulières ou QR (par défaut avec "lm") pour inverser la matrice
+isTRUE(all.equal(
+  betas,
+  mco(X = X, y = y)))
+
+# Autres quantités d'intérêt
+residus <- resid(linmod)
+# Estimation de l'écart-type sigma, avec n-p-1 comme dénominateur
+sd_linmod <- sqrt(sum(residus^2)/(length(y) - length(betas)))
+# Matrice de covariance des paramètres de la moyenne
+vcov_beta <- vcov(linmod)
+# Extraire les erreurs-types des betas
+se_beta <- sqrt(diag(vcov_beta))
+isTRUE(all.equal(
+  sd_linmod^2 * solve(t(X) %*% X),
+  vcov_beta))
+
+# Valeurs ajustées - prédictions
+val_ajust <- fitted(linmod)
+# Valeur du R2 (coefficient de détermination)
+# carré de la corrélation linéaire entre y et ypred
+cor(val_ajust, y)^2
